5df082894dbc5a3e9259cee1b9858509ef8a9184
Bushra haider <bushrah.3003@gmail.com>
2023-12-22 15:50:56 +0000

    added JavaScript to the weather icon

--- index.js
+++ index.js
@@ -5,15 +5,39 @@ function searchWeather(response){
     let descriptionElement = document.querySelector("#weather-description");
     let humidityElement = document.querySelector("#weather-humidity");
     let windElement = document.querySelector("#weather-wind-speed");
-
+    let timeElement = document.querySelector("#weather-time");
+    let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#weather-icon");
 
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-    windElement.innerHTML = `${response.data.wind.speed}km/hr`
+    windElement.innerHTML = `${response.data.wind.speed}km/hr`;
+    timeElement.innerHTML = timeDate(date);
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
     }
 
+    function timeDate(date){
+        let minutes = date.getMinutes();
+        let hours = date.getHours();
+        let days = [
+          "Sunday",
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+        ];
+        let day = days[date.getDay()];
+      
+        if (minutes < 10) {
+          minutes = `0${minutes}`;
+        }
+      
+        return `${day} ${hours}:${minutes}`;
+    }
     
     function searchCity(city){
         let apiKey = "a4oe8fb84b2d8f83f294f146t0af1032";
